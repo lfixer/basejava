7d76219a180c272f7d23e26bee8afb0c48a04194
lfixer <strmgddn@gmail.com>
2020-10-08 12:10:56 +0300

    clear, save, get, delete, getAll, size methods are implemented

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,49 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int ResumeCounter;
 
     void clear() {
+        for (int i = 0; i < ResumeCounter; i++) {
+            storage[i] = null;
+        }
+        ResumeCounter = 0;
     }
 
     void save(Resume r) {
+        storage[ResumeCounter] = r;
+        ResumeCounter++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < ResumeCounter; i++) {
+            if (storage[i].uuid.equals(uuid))
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int i = 0;
+        while (!storage[i].uuid.equals(uuid)) {
+            i++;
+        }
+        storage[i] = null;
+        if (ResumeCounter - i + 1 >= 0) System.arraycopy(storage, i + 1, storage, i + 1 - 1, ResumeCounter - i + 1);
+        storage[ResumeCounter - 1] = null;
+        ResumeCounter--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] ResumesWithoutNulls = new Resume[ResumeCounter];
+        if (ResumeCounter >= 0) System.arraycopy(storage, 0, ResumesWithoutNulls, 0, ResumeCounter);
+        return ResumesWithoutNulls;
     }
 
     int size() {
-        return 0;
+        return (ResumeCounter);
     }
 }
